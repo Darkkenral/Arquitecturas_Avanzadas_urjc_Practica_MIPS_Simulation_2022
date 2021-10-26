1ed963f035451e0b087a90bc773cf200ad21cfa5
Miguel <miguelalbertolucea@hotmail.es>
2021-10-26 14:58:27 +0200

    14:58

--- MipsSimulator.py
+++ MipsSimulator.py
@@ -3,7 +3,7 @@ import collections
 
 
 class Segmentacionsimulacion(object):
-    operations = {'add': '+', 'addi': '+', 'sub': '-', 'subi': '-',
+    operaciones = {'add': '+', 'addi': '+', 'sub': '-', 'subi': '-',
                   'and': '&', 'andi': '&', 'or': '|', 'ori': '|', 'mul': '*'}
 
     def __init__(self, listaInstrucciones):
@@ -14,14 +14,16 @@ class Segmentacionsimulacion(object):
         self.ramificado = False
         self.aparcado = False
         self.registros = {}
-
+    
         self.segmentacion = [None for x in range(0, 5)]
 
-        self.segmentacion[0] = FetchStage(Instruccion_vacia, self)
-        self.segmentacion[1] = WriteStage(Instruccion_vacia, self)
-        self.segmentacion[2] = ReadStage(Instruccion_vacia, self)
-        self.segmentacion[3] = ExecStage(Instruccion_vacia, self)
-        self.segmentacion[4] = DataStage(Instruccion_vacia, self)
+        
+
+        self.segmentacion[0] = FetchStage( Instruccion_vacia(), self)
+        self.segmentacion[1] = WriteStage(Instruccion_vacia(), self)
+        self.segmentacion[2] = ReadStage(Instruccion_vacia(), self)
+        self.segmentacion[3] = ExecStage(Instruccion_vacia(), self)
+        self.segmentacion[4] = DataStage(Instruccion_vacia(), self)
 
         # Generacion de la memoria de los registros
         self.registros['$zero'] = 0
@@ -74,14 +76,14 @@ class Segmentacionsimulacion(object):
 
         self.segmentacion[1] = WriteStage(self.segmentacion[4].instr, self)
         if self.aparcado:
-            self.segmentacion[4] = DataStage(Instruccion_vacia, self)
+            self.segmentacion[4] = DataStage(Instruccion_vacia(), self)
             self.aparcado = False
         else:
             self.segmentacion[4] = DataStage(self.segmentacion[3].instr, self)
             self.segmentacion[3] = ExecStage(self.segmentacion[2].instr, self)
             self.segmentacion[2] = ReadStage(self.segmentacion[0].instr, self)
-            self.segmentacion[0] = FetchStage(Instruccion_vacia, self)
-        self.debug()
+            self.segmentacion[0] = FetchStage(Instruccion_vacia(), self)
+
         # Avanzamos en la segmentacion en cada uno de los pasos.
         for pi in self.segmentacion:
             pi.advance()
@@ -110,7 +112,7 @@ class Segmentacionsimulacion(object):
         """Ejecuta la simulacion, hasta que la señal done se activa"""
         while not self.__terminado:
             self.step()
-            self.debug()
+            #self.debug()
 
     def getForwardVal(self, regnombre):
         """ Realiza el forwarding basandose en el nombre del registro
@@ -126,10 +128,10 @@ class Segmentacionsimulacion(object):
         else:
             return "NULL"
 
-    ### DEBUGGING INFORMATION PRINTING ###
+    ### ESTADO DE LA SIMULACION ###
     def debug(self):
         print("######################## debug ###########################")
-        self.printStageCollection()
+        self.printColeccionEstado()
         self.printRegFile()
         print("\n<contador_de_programa>", self.contador_de_programa)
         self.printsegmentacion()
@@ -149,8 +151,8 @@ class Segmentacionsimulacion(object):
         for k, v in self.registros.items():
                 print("\n", k, " : ", v,)
 
-    def printStageCollection(self):
-        print("<Instruccion Collection>")
+    def printColeccionEstado(self):
+        print("<Coleccion de instrucciones>")
         for index, item in (self.memoria_principal.items()):
             if item != 0:
                 print(index, ": ", str(item))
@@ -192,7 +194,7 @@ class FetchStage(segmentacionStage):
         self.simulacion.contador_de_programa += 4
 
     def __str__(self):
-        return 'Fetch Stage\t' + ':\t' + str(self.instr)
+        return 'FetchStage \t' + ':\t' + str(self.instr)
 
 
 class ReadStage(segmentacionStage):
@@ -202,30 +204,30 @@ class ReadStage(segmentacionStage):
 
     def advance(self):
         """
-        Read the necessary registers from the registers file
-        used in this instruction
+        Lee el dato de los registros utilizados en la instruccion
         """
-        if((type(self.instr) != type(Instruccion_vacia)) is not (type(self.instr) != type(Etiqueta))):
-            if(self.instr.regRead):
-                self.instr.reg_value_s = self.simulacion.registros[self.instr.reg_nombre_s]
-                if (hasattr(self.instr, 'inmediato')):
-                    # son de tipo I
-                    if self.instr.op == 'beq' or self.instr.op == 'bne':
-                        self.instr.reg_value_s = self.simulacion.registros[self.instr.reg_nombre_s]
+        if self.instr.__class__.__name__ != Instruccion_vacia.__name__:
+            if self.instr.__class__.__name__ != Etiqueta.__name__:
+                if(self.instr.regRead):
+                    self.instr.reg_value_s = self.simulacion.registros[self.instr.reg_nombre_s]
+                    if (hasattr(self.instr, 'inmediato')):
+                        # son de tipo I
+                        if self.instr.op == 'beq' or self.instr.op == 'bne':
+                            self.instr.reg_value_s = self.simulacion.registros[self.instr.reg_nombre_s]
+                            self.instr.reg_value_t = self.simulacion.registros[self.instr.reg_nombre_t]
+                        if self.instr.op == 'lw' or self.instr.op == 'sw':
+                            self.instr.reg_value_t = self.simulacion.registros[self.instr.reg_nombre_t]
+                    elif hasattr(self.instr, 'reg_nombre_t'):
                         self.instr.reg_value_t = self.simulacion.registros[self.instr.reg_nombre_t]
-                    if self.instr.op == 'lw' or self.instr.op == 'sw':
-                        self.instr.reg_value_t = self.simulacion.registros[self.instr.reg_nombre_t]
-                elif hasattr(self.instr, 'reg_nombre_t'):
-                    self.instr.reg_value_t = self.simulacion.registros[self.instr.reg_nombre_t]
 
-            if self.instr.op == 'j':
-                self.simulacion.contador_de_programa = self.instr.target
-                # Set the other instructions currently in the pipeline to a Instruccion_vacia
-                self.simulacion.pipeline[0] = FetchStage(
-                    Instruccion_vacia, self)
+                if self.instr.op == 'j':
+                    self.simulacion.contador_de_programa = self.instr.target
+                    # Pone el resto de instrucciones del pipeline a instruccion vacia en caso de salto
+                    self.simulacion.pipeline[0] = FetchStage(
+                        Instruccion_vacia, self)
 
     def __str__(self):
-        return 'Read from Register'+ ':\t' + str(self.instr)
+        return 'ReadStage '+ ':\t' + str(self.instr)
 
 
 class ExecStage(segmentacionStage):
@@ -235,82 +237,84 @@ class ExecStage(segmentacionStage):
 
     def advance(self):
         """
-        Execute the instruction according to its mapping of
-        assembly operation to Python operation
+        Ejecuta la operacion en funcion de su campo op, se utilizan las coleccion de operaciones 
         """
+        print(str(self.simulacion.registros))
 
-        if((type(self.instr) != type(Instruccion_vacia)) is not (type(self.instr) != type(Etiqueta))):
-            if self.instr.aluop:
-                # Si tenemos un posible peligro en el registro s o en el t
-                # cogemos el valor foward
+        
+        if self.instr.__class__.__name__ != Instruccion_vacia.__name__:
+            if self.instr.__class__.__name__ != Etiqueta.__name__:
+                if self.instr.aluop:
+                    # Si tenemos un posible peligro en el registro s o en el t
+                    # cogemos el valor foward
 
-                if self.instr.reg_nombre_s in self.simulacion.listaPeligros:
-                    forwardVal = self.simulacion.getForwardVal(
-                        self.instr.reg_nombre_s)
-                    if forwardVal != "NULL":
-                        self.instr.reg_value_s = forwardVal
+                    if self.instr.reg_nombre_s in self.simulacion.listaPeligros:
+                        forwardVal = self.simulacion.getForwardVal(
+                            self.instr.reg_nombre_s)
+                        if forwardVal != "NULL":
+                            self.instr.reg_value_s = forwardVal
+                        else:
+                            self.simulacion.aparcado = True
+                            return
+                    if self.instr.reg_nombre_t in self.simulacion.listaPeligros:
+                        forwardVal = self.simulacion.getForwardVal(
+                            self.instr.reg_nombre_t)
+                        if forwardVal != "NULL":
+                            self.instr.reg_value_t = forwardVal
+                        else:
+                            self.simulacion.aparcado = True
+                            return
+
+                    # Annadimos el registro de destino a la lista de peligros.
+                        if ha
+                        if self.instr.regWrite:
+                            self.simulacion.listaPeligros.append(
+                                self.instr.reg_nombre_dest)
+
+        
+                    if self.instr.op == 'lw':
+                        self.instr.reg_value_s = self.simulacion.memoria_principal[self.instr.reg_value_t+self.instr.value]
+                    elif self.instr.op == 'li':
+                        self.instr.value = self.instr.inmediato
+                        self.simulacion.registros[self.instr.reg_nombre_s]=self.instr.inmediato
+                    elif self.instr.op == 'sw':
+                        self.instr.reg_value_t = self.simulacion.memoria_principal[self.instr.reg_value_s+self.instr.value]
+                    elif self.instr.op == 'bne':
+                        if self.instr.reg_value_s != self.instr.reg_value_t:
+                            # Colocamos el contador de programa a la etiqueta
+                            self.simulacion.contador_de_programa = self.instr.inmediato
+                            # Ponemos el resto de instrucciones del pipeline a Instruccion_vacias
+                            self.simulacion.pipeline[0] = FetchStage(
+                                Instruccion_vacia, self)
+                            self.simulacion.pipeline[2] = ReadStage(
+                                Instruccion_vacia, self)
+                            #activamos la señal de salto
+                            self.simulacion.branched = True
+                    elif self.instr.op == 'beq':
+                        if self.instr.reg_value_s == self.instr.reg_value_t:
+                            # Colocamos el contador de programa a la etiqueta
+                            self.simulacion.contador_de_programa = self.instr.inmediato
+                            # Ponemos el resto de instrucciones del pipeline a Instruccion_vacias
+                            self.simulacion.pipeline[0] = FetchStage(
+                                Instruccion_vacia, self)
+                            self.simulacion.pipeline[2] = ReadStage(
+                                Instruccion_vacia, self)
+                            self.simulacion.branched = True
                     else:
-                        self.simulacion.aparcado = True
-                        return
-                if self.instr.reg_nombre_t in self.simulacion.listaPeligros:
-                    forwardVal = self.simulacion.getForwardVal(
-                        self.instr.reg_nombre_t)
-                    if forwardVal != "NULL":
-                        self.instr.reg_value_t = forwardVal
-                    else:
-                        self.simulacion.aparcado = True
-                        return
-
-                # Annadimos el registro de destino a la lista de peligros.
-                if self.instr.regWrite:
-                    self.simulacion.listaPeligros.append(
-                        self.instr.reg_nombre_dest)
-
-    
-                if self.instr.op == 'lw':
-                    self.instr.reg_value_s = self.simulacion.memoria_principal[self.instr.reg_value_t+self.instr.value]
-                elif self.instr.op == 'li':
-                    self.instr.value = self.instr.inmediato
-                    self.simulacion.registros[self.instr.reg_nombre_s]=self.instr.inmediato
-                elif self.instr.op == 'sw':
-                    self.instr.reg_value_t = self.simulacion.memoria_principal[self.instr.reg_value_s+self.instr.value]
-                elif self.instr.op == 'bne':
-                    if self.instr.reg_value_s != self.instr.reg_value_t:
-                        # Colocamos el contador de programa a la etiqueta
-                        self.simulacion.contador_de_programa = self.instr.inmediato
-                        # Ponemos el resto de instrucciones del pipeline a Instruccion_vacias
-                        self.simulacion.pipeline[0] = FetchStage(
-                            Instruccion_vacia, self)
-                        self.simulacion.pipeline[2] = ReadStage(
-                            Instruccion_vacia, self)
-                        #activamos la señal de salto
-                        self.simulacion.branched = True
-                elif self.instr.op == 'beq':
-                    if self.instr.reg_value_s == self.instr.reg_value_t:
-                        # Colocamos el contador de programa a la etiqueta
-                        self.simulacion.contador_de_programa = self.instr.inmediato
-                           # Ponemos el resto de instrucciones del pipeline a Instruccion_vacias
-                        self.simulacion.pipeline[0] = FetchStage(
-                            Instruccion_vacia, self)
-                        self.simulacion.pipeline[2] = ReadStage(
-                            Instruccion_vacia, self)
-                        self.simulacion.branched = True
-                else:
-                    if (self.instr.op == 'slt'):
-                        val = 1 if self.instr.reg_value_s < self.instr.reg_value_t else 0
-                        self.instr.value = val
-                        self.simulacion.registros[self.instr.reg_nombre_dest]=val
-                    elif (self.instr.op == 'nor'):
-                        self.instr.value = (self.instr.reg_value_s | self.instr.reg_value_t)
-                        self.simulacion.registros[self.instr.reg_nombre_dest]=  self.instr.value
-                    else:
-                        self.instr.value = eval("%d %s %d" %
-                                                (self.instr.reg_value_s,
-                                                self.simulacion.operations[self.instr.op],
-                                                self.instr.reg_value_t))
-
+                        if (self.instr.op == 'slt'):
+                            val = 1 if self.instr.reg_value_s < self.instr.reg_value_t else 0
+                            self.instr.value = val
+                            self.simulacion.registros[self.instr.reg_nombre_dest]=val
+                        elif (self.instr.op == 'nor'):
+                            self.instr.value = (self.instr.reg_value_s | self.instr.reg_value_t)
+                            self.simulacion.registros[self.instr.reg_nombre_dest]=  self.instr.value
+                        else:
+                            self.instr.value = eval("%d %s %d" %
+                                                    (self.instr.reg_value_s,
+                                                    self.simulacion.operaciones[self.instr.op],
+                                                    self.instr.reg_value_t))
     def __str__(self):
-        return 'Execute Stage\t'+ ':\t' + str(self.instr)
+        return 'ExecStage '+ ':\t' + str(self.instr)
 
 
 class DataStage(segmentacionStage):
@@ -320,17 +324,19 @@ class DataStage(segmentacionStage):
 
     def advance(self):
         """
-        If we have to write to main memory, write it first
-        and then read from main memory second
+        Si tenemos que leer de memoria , escribe primero
+        y despues lee de memoria principal
         """
-        if((type(self.instr) != type(Instruccion_vacia)) is not (type(self.instr) != type(Etiqueta))):
-            if self.instr.writeMem:
-                self.simulacion.mainmemory[self.instr.reg_value_t] = self.instr.reg_value_s
-            elif self.instr.readMem:
-                self.instr.value = self.simulacion.mainmemory[self.instr.reg_value_s]
+   
+        if self.instr.__class__.__name__ != Instruccion_vacia.__name__:
+            if self.instr.__class__.__name__ != Etiqueta.__name__:
+                if self.instr.writeMem:
+                    self.simulacion.mainmemory[self.instr.reg_value_t] = self.instr.reg_value_s
+                elif self.instr.readMem:
+                    self.instr.value = self.simulacion.mainmemory[self.instr.reg_value_s]
 
     def __str__(self):
-        return 'Main Memory'
+        return 'DataStage  '
 
 
 class WriteStage(segmentacionStage):
@@ -340,12 +346,14 @@ class WriteStage(segmentacionStage):
 
     def advance(self):
         """
-        Write to the register file
+        Escribimos en la memoria de registros 
         """
-        if((type(self.instr) != type(Instruccion_vacia)) is not (type(self.instr) != type(Etiqueta))):
-            if self.instr.regWrite:
-                if hasattr(self.instr, 'reg_nombre_dest'):
-                    self.simulacion.registers[self.instr.reg_nombre_dest] = self.instr.value
+
+        if self.instr.__class__.__name__ != Instruccion_vacia.__name__:
+            if self.instr.__class__.__name__ != Etiqueta.__name__:
+                if self.instr.regWrite:
+                    if hasattr(self.instr, 'reg_nombre_dest'):
+                        self.simulacion.registers[self.instr.reg_nombre_dest] = self.instr.value
 
     def __str__(self):
-        return 'Write to Register'+ ':\t' + str(self.instr)
+        return 'WriteStage '+ ':\t' + str(self.instr)
